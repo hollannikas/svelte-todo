715b58cb74c77a884995ce653d2603e77404d9f7
Rudolf Poels <hollannikas@users.noreply.github.com>
2023-11-29 20:02:48 +0200

    Prettier

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-    import {createEventDispatcher} from 'svelte';
-    const dispatch = createEventDispatcher();
-	import type { ITodo } from "./types";
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	import type { ITodo } from './types';
 
-    export let todo: ITodo;
+	export let todo: ITodo;
 </script>
 
 <li>
-    <input type="checkbox" bind:checked={todo.completed} />
-    <span class:completed={todo.completed}>{todo.title}</span>
-    <button on:click={() => dispatch('delete')}>Delete</button>
+	<input type="checkbox" bind:checked={todo.completed} />
+	<span class:completed={todo.completed}>{todo.title}</span>
+	<button on:click={() => dispatch('delete')}>Delete</button>
 </li>
 
 <style>
-    .completed {
-        text-decoration: line-through;	
-    }
+	.completed {
+		text-decoration: line-through;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
 	import type { ITodo } from '$lib/types';
-    import Todo from '$lib/Todo.svelte';
+	import Todo from '$lib/Todo.svelte';
 
-    let todos: ITodo[] = [
-        { id: 1, title: 'Do the dishes', completed: false },
-        { id: 2, title: 'Take out the trash', completed: false },
-        { id: 3, title: 'Mow the lawn', completed: false }
-    ];
+	let todos: ITodo[] = [
+		{ id: 1, title: 'Do the dishes', completed: false },
+		{ id: 2, title: 'Take out the trash', completed: false },
+		{ id: 3, title: 'Mow the lawn', completed: false }
+	];
 
-    let title = '';
+	let title = '';
 
-    const addTodo = () => {
-        todos = [...todos, { id: todos.length + 1, title, completed: false }];
-        title = '';
-    };
+	const addTodo = () => {
+		todos = [...todos, { id: todos.length + 1, title, completed: false }];
+		title = '';
+	};
 
-    const deleteTodo = (id: number) => {
-        todos = todos.filter(todo => todo.id !== id);
-    };
+	const deleteTodo = (id: number) => {
+		todos = todos.filter((todo) => todo.id !== id);
+	};
 </script>
 
 <h1>TODOs</h1>
 
 <ul>
-    {#each todos as todo}
-        <Todo {todo} on:delete={() => deleteTodo(todo.id)}/>
-    {/each}
+	{#each todos as todo}
+		<Todo {todo} on:delete={() => deleteTodo(todo.id)} />
+	{/each}
 </ul>
 
-<input bind:value={title}/><button on:click={addTodo}>Add</button>
+<input bind:value={title} /><button on:click={addTodo}>Add</button>
 
 <style>
-    ul {
-        list-style: none;
-        padding: 0;
-    }
+	ul {
+		list-style: none;
+		padding: 0;
+	}
 </style>
