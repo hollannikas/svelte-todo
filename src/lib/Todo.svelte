503ccf6440e6bc50992ab7fc498fa6b61ae5e723
Rudolf Poels <hollannikas@users.noreply.github.com>
2023-11-29 19:14:44 +0200

    Strike through completed

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -4,7 +4,13 @@
     export let todo: ITodo;
 </script>
 
-<div>
+<div class:completed={todo.completed}>
     <input type="checkbox" bind:checked={todo.completed} />
     {todo.title}
 </div>
+
+<style>
+    .completed {
+        text-decoration: line-through;	
+    }
+</style>
