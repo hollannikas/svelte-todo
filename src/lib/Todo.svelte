1310e58be0fa71151f211f31d355a7ccaf23e99c
Rudolf Poels <hollannikas@users.noreply.github.com>
2023-11-29 19:05:36 +0200

    Add todo

---
+++ src/lib/Todo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { ITodo } from "./types";
+
+    export let todo: ITodo;
+</script>
+
+<div>
+    <input type="checkbox" bind:checked={todo.completed} />
+    {todo.title}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { tick } from 'svelte';
+	import type { ITodo } from '$lib/types';
+    import Todo from '$lib/Todo.svelte';
+
+    let todos: ITodo[] = [
+        { id: 1, title: 'Do the dishes', completed: false },
+        { id: 2, title: 'Take out the trash', completed: false },
+        { id: 3, title: 'Mow the lawn', completed: false }
+    ];
+
+    let title = '';
+
+    const addTodo = async() => {
+        todos = [...todos, { id: todos.length + 1, title, completed: false }];
+        title = '';
+    };
+</script>
+
+<h1>TODOs</h1>
+{#each todos as todo}
+    <Todo todo={todo} />
+{/each}
+
+<input bind:value={title}/><button on:click={addTodo}>Add</button>
+
